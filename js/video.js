a980cbdc3b76f61768d81b689b43b34723891b21
yexiaosu <wang.lan313@sjtu.edu.cn>
2022-11-22 22:53:29 -0600

    Play and pause

--- js/video.js
+++ js/video.js
@@ -1,7 +1,16 @@
 // Add js here.
-window.onload = function() {
-    var vid = document.getElementById("videoplayer");
-    vid.loop = false;
-    vid.autoplay = false;
-    vid.load();
-  };
+var vid = document.getElementById("videoplayer");
+
+window.onload = function () {
+  vid.loop = false;
+  vid.autoplay = false;
+  vid.load();
+};
+
+document.getElementById("play").onclick = function() {
+    vid.play();
+};
+
+document.getElementById("pause").onclick = function() {
+    vid.pause();
+};
